6e06e594a98e1d76422588186732a54c570a20a6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-12 14:54:16 +0100

    Implemented the pseudocode

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,32 +20,25 @@ class CPU:
         # initialize address to zero
         address = 0
 
-        # remove the program list
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
         # open file name using the with command
+        with open(filename, "r") as f:
             # loop through every line in f
+            for line in f:
                 # split the line on an #
+                split_line = line.split("#")
                 # initialize command to the left item in the split operation
                 # and call strip on it
+                command = split_line[0].strip()
                 # check if command is an empty string
-                    # continue
+                if command == "":
+                    # it's a comment, continue
+                    continue
                 # convert the binary command to integer using the int function
+                command = int(command, 2)
                 # add command to self.ram at index address
+                self.ram[address] = command
                 # increment address
-
-        # remove this block of code
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
